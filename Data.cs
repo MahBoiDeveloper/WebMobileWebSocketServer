a5b521a0886992ef62c5c93212db6fdd43e07cb2
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-10-12 19:59:32 +0300

    Add array into answer on get

--- Data.cs
+++ Data.cs
@@ -7,11 +7,18 @@ public struct Data
     public Int32 pageViews { get; set; }
     public Int32 uniqueVisitors { get; set; }
     public Int32 avgSessionDuration { get; set; }
+    public List<Int32> pageViewsPerDay { get; set; }
 
     public Data(Random rng)
     {
         uniqueVisitors = (Int32)(rng.NextInt64(10, Int32.MaxValue)) % 1000;
         pageViews = uniqueVisitors + (Int32)(rng.NextInt64(10, Int32.MaxValue)) % 300;
-        avgSessionDuration = (Int32)(rng.NextInt64(10, Int32.MaxValue)) % 10000;
+        avgSessionDuration = (Int32)(rng.NextInt64(100, Int32.MaxValue)) % 10000;
+        pageViewsPerDay = [ 0, 0, 0, 0, 0, 0, 0 ];
+        pageViewsPerDay[0] = pageViews;
+        for (int i = 1; i < 7; i++)
+        {
+            pageViewsPerDay[i] = pageViewsPerDay[i - 1] + (Int32)(rng.NextInt64(10, Int32.MaxValue)) % 100;
+        }
     }
 }
