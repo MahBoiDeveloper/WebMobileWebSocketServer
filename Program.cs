60875ea356abb1f9dc1605f0df11a1e1347ee855
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-10-13 12:46:50 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -11,41 +11,48 @@ public class Program
         string certificatePath = "/etc/letsencrypt/live/expserver.site/fullchain.pem";
         string privateKeyPath = "/etc/letsencrypt/live/expserver.site/privkey.pem";
 
-        if (!new FileInfo(certificatePath).Exists)
+        try
         {
-            Console.WriteLine($"[Warning] File {certificatePath} doens't found!");
-        }
-
-        if (!new FileInfo(privateKeyPath).Exists)
-        {
-            Console.WriteLine($"[Warning] File {privateKeyPath} doens't found!");
-        }
-
-        X509Certificate2 certificate = new(certificatePath, privateKeyPath);
-
-        if (!certificate.Verify())
-        {
-            Console.WriteLine($"[Warning] Certificate isn't verified!");
-        }
-        WebSocketServer wss = new("wss://0.0.0.0:41000")
-        {
-            EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12,
-        };
-        wss.Start(con =>
-        {
-            con.OnMessage = msg => 
+            if (!new FileInfo(certificatePath).Exists)
             {
-                string ret = msg switch
-                {
-                    "get" => JsonSerializer.Serialize<Data>(new Data(rng)),
-                    _ => $"[{nameof(wss)}] Message len: " + msg.Length
-                };
-                con.Send(ret);
-                Console.WriteLine("Sending on get request: " + ret);
+                Console.WriteLine($"[Warning] File {certificatePath} doens't found!");
+            }
+
+            if (!new FileInfo(privateKeyPath).Exists)
+            {
+                Console.WriteLine($"[Warning] File {privateKeyPath} doens't found!");
+            }
+
+            X509Certificate2 cert = new(certificatePath, privateKeyPath);
+
+            if (!cert.Verify())
+            {
+                Console.WriteLine($"[Warning] Certificate isn't verified!");
+            }
+            WebSocketServer wss = new("wss://0.0.0.0:41000")
+            {
+                EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12,
+                Certificate = cert
             };
-            con.OnOpen = () => con.Send($"[{nameof(wss)}] Greetings!");
-            con.OnClose = () => con.Send($"[{nameof(wss)}] Goodbye!");
-        });
+            wss.Start(con =>
+            {
+                con.OnMessage = msg =>
+                {
+                    string ret = msg switch
+                    {
+                        "get" => JsonSerializer.Serialize<Data>(new Data(rng)),
+                        _ => $"[{nameof(wss)}] Message len: " + msg.Length
+                    };
+                    con.Send(ret);
+                    Console.WriteLine("Sending on get request: " + ret);
+                };
+                con.OnOpen = () => con.Send($"[{nameof(wss)}] Greetings!");
+                con.OnClose = () => con.Send($"[{nameof(wss)}] Goodbye!");
+            });
+        }
+        catch
+        {
+        }
 
         WebSocketServer ws = new("ws://0.0.0.0:40000");
         ws.Start(con =>
@@ -55,13 +62,13 @@ public class Program
                 string ret = msg switch
                 {
                     "get" => JsonSerializer.Serialize<Data>(new Data(rng)),
-                    _ => $"[{nameof(wss)}] Message len: " + msg.Length
+                    _ => $"[{nameof(ws)}] Message len: " + msg.Length
                 };
                 con.Send(ret);
                 Console.WriteLine("Sending on get request: " + ret);
             };
-            con.OnOpen = () => con.Send($"[{nameof(wss)}] Greetings!");
-            con.OnClose = () => con.Send($"[{nameof(wss)}] Goodbye!");
+            con.OnOpen = () => con.Send($"[{nameof(ws)}] Greetings!");
+            con.OnClose = () => con.Send($"[{nameof(ws)}] Goodbye!");
         });
 
         WebApplication.CreateBuilder(args).Build().Run();
